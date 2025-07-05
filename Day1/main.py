2365094693b18748868661664c782dc8552e47b0
Ashok-Negi <ashoknegi1394@gmail.com>
2025-07-05 09:12:18 +0530

    adding Day 1 main.py

---
+++ Day1/main.py
@@ -0,0 +1,9 @@
+# main.py
+print("Hello, world!")
+print("Enter your City name:")
+city = input()
+print("Enter your favoutite animal name:")
+animal = input()
+print("the band name that you can use is: " + city + " " + animal)
+# This is a simple Python script that prints "Hello, world!" to the console.
+# It serves as a basic introduction to Python syntax and execution.
